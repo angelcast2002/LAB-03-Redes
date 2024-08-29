ad1e19f84abe5ac2af7043e63565ba935dca871d
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2024-08-29 01:08:04 -0600

    Update LSR.js
    Co-Authored-By: Diego Morales <77738746+Diego2250@users.noreply.github.com>
    Co-Authored-By: Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>

--- LSR/LSR.js
+++ LSR/LSR.js
@@ -5,97 +5,117 @@ const { client, xml } = require("@xmpp/client");
 let xmppClient;
 
 const connect = async (Username, password) => {
-  try {
-    xmppClient = client({
-      service: "ws://alumchat.lol:7070/ws",
-      domain: "alumchat.lol",
-      resource: "ReactNative",
-      username: Username,
-      password,
-    });
-
-    xmppClient.on("online", async (address) => {
-      console.log("Connected as", address.toString());
-      await xmppClient.send(
-        xml(
-          "presence",
-          {},
-          xml("status", {}, `Hola soy ${Username}`),
-          xml("show", {}, "chat")
-        )
-      );
-    });
-
-    xmppClient.on("stanza", async (stanza) => {
-      try {
-        console.log("Stanza received:", stanza.toString());
-        if (stanza.is("message")) {
-          const type = stanza.attrs.type;
-          if (type === "echo") {
-            console.log("Echo stanza received");
-          }
-        }
-      } catch (error) {
-        console.error("Failed to process stanza:", error);
-        throw new Error("Failed to process stanza");
-      }
-    });
-    await xmppClient.start();
-  } catch (err) {
-    console.error("Failed to connect:", err);
-  }
+    try {
+        xmppClient = client({
+            service: "ws://alumchat.lol:7070/ws",
+            domain: "alumchat.lol",
+            resource: "ReactNative",
+            username: Username,
+            password,
+        });
+        
+        xmppClient.on("online", async (address) => {
+            console.log("Connected as", address.toString());
+            await xmppClient.send(
+                xml(
+                    "presence",
+                    {},
+                    xml("status", {}, `Hola soy ${Username}`),
+                    xml("show", {}, "chat")
+                )
+            );
+        });
+        
+        xmppClient.on("stanza", async (stanza) => {
+            try {
+                //console.log("Stanza received:", stanza);
+                if (stanza.is("message")) {
+                    console.log("Stanza message received", stanza.toString());
+                    const type = stanza.attrs.type;
+                    if (type === "chat") {
+                        const body = stanza.getChildText("body");
+                        if (body) {
+                            const jsonBody = JSON.parse(body);
+                            if (jsonBody.type === "echo") {
+                                console.log("Echo stanza received from", jsonBody.from);
+                                const neighbors = getContacts();
+                                const neighbors_stanza = xml("neighbors", {});
+                                await send_neighbors_stanza(neighbors_stanza);
+                            }
+                        }
+                    }
+                }
+            } catch (error) {
+                console.error("Failed to process stanza:", error);
+                throw new Error("Failed to process stanza");
+            }
+        });
+        await xmppClient.start();
+    } catch (err) {
+        console.error("Failed to connect:", err);
+    }
 };
 
 const logOut = async () => {
-  if (xmppClient) {
-    try {
-      await xmppClient.send(xml("presence", { type: "unavailable" }));
-      await xmppClient.stop();
-    } catch (error) {
-      console.error("Failed to log out:", error);
+    if (xmppClient) {
+        try {
+            await xmppClient.send(xml("presence", { type: "unavailable" }));
+            await xmppClient.stop();
+        } catch (error) {
+            console.error("Failed to log out:", error);
+        }
     }
-  }
 };
 
 /**
- * Pedir los contactos
- */
-const getContacts = async () => {
-  return [
-    ["mor21146@alumchat.lol", 1],
-    ["contact2@alumchat.lol", 1],
-    ["contact3@alumchat.lol", 1]
-  ];
+* Pedir los contactos
+*/
+const getContacts = () => {
+    return [
+        ["azu21242@alumchat.lol", 1],
+        ["contact2@alumchat.lol", 1],
+        ["contact3@alumchat.lol", 1],
+    ];
 };
 
 /**
- * pedir la información de los contactos de nuestros contactos y sus pesos. 
- * Con una stanza personalizada, de tipo echo, que contenga el nombre de usuario y el peso.
- */
-const getNeighbors = async (contact) => {
-  // generar stanza
-  const request_neighbors_stanza = xml(
-    "message",
-    { type: "echo", to: contact }
-  );
-  await xmppClient.send(request_neighbors_stanza);
-}
+* pedir la información de los contactos de nuestros contactos y sus pesos.
+* Con una stanza personalizada, de tipo echo, que contenga el nombre de usuario y el peso.
+*/
+const getNeighbors = async (contact, username) => {
+    const body = {
+        type: "echo",
+        from: username,
+    };
+    const request_neighbors_stanza = xml(
+        "message",
+        { type: "chat", to: contact },
+        xml("body", {}, JSON.stringify(body))
+    );
+    
+    await xmppClient.send(request_neighbors_stanza);
+};
 
-const send_neighbors_stanza = async(contact) => {
-  const neighbors = await getContacts();
-  neighbors.forEach((neighbor) => {
-    const neighbor_stanza = xml("neighbor", {}, neighbor[0], neighbor[1]);
-    contact.append(neighbor_stanza);
-  });
-  await xmppClient.send(contact);
-}
-  
+const send_neighbors_stanza = async (contact) => {
+    const neighbors = await getContacts();
+    neighbors.forEach((neighbor) => {
+        const neighbor_stanza = xml("neighbor", {}, neighbor[0], neighbor[1]);
+        contact.append(neighbor_stanza);
+    });
+    await xmppClient.send(contact);
+};
 
 const main = async () => {
-  const Username = "mor21146";
-  const password = "1234";
-
-  await connect(Username, password);
+    const Username = "mor21146";
+    const password = "1234";
+    
+    await connect(Username, password);
+    const contacts = await getContacts();
+    
+    for (let i = 0; i < contacts.length; i++) {
+        console.log("Contacto: ", contacts[i][0]);
+        await getNeighbors(contacts[i][0], `${Username}@alumchat.lol`);
+    }
 };
 
 main().catch((err) => console.error("Error en la ejecución:", err));
